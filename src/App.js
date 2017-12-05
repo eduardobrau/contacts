91b3ce00cd822e4bde08003383de4d49db7b5532
Esteves <esteves@edusites.com.br>
2017-12-05 21:52:59 -0200

    Modificações nos arquivos padrões do react-app: Deletado src/App.css e logo.svg, excluido conteudo de src/App.js e index.css

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React, { Component } from 'react';
 
-class ContactList extends React.Component {
-  
-  render() {
-    /* 2º Ao invés de atribuir uma lista de contatos
-    a people foi adicionado a propriedade this.props 
-    na estrutura de cada Component e assim podemos atribuir
-    seus valores em tempo de execução. */
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-
-}
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      {/* 1º Adicionado a propriedade props no component
-      ContactList para configurar cada elemento da lista
-      dinamicamente */}
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Ryan' },
-          { name: 'Tyler' }
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Maria' },
-          { name: 'Larissa' }
-        ]}/>
-      </div>
-    );
+    
   }
 }
 
