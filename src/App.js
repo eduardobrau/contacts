02dff7331bdb2225b3e21bbd4e4d626e52c3135a
Esteves <esteves@edusites.com.br>
2017-12-05 19:30:34 -0200

    Adicionado a propriedade this.props no component ContactList, dando a flexibilidade de atribuir cada valor da lista de contatos dinamicamente em tempo de execução e uma maior flexibilidade em reuso.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import React, { Component } from 'react';
 class ContactList extends React.Component {
   
   render() {
-    
-    const people = [
-      { name: 'Michael' },
-      { name: 'Ryan' },
-      { name: 'Tyler' }
-    ]
+    /* 2º Ao invés de atribuir uma lista de contatos
+    a people foi adicionado a propriedade this.props 
+    na estrutura de cada Component e assim podemos atribuir
+    seus valores em tempo de execução. */
+    const people = this.props.contacts;
 
     return <ol>
       {people.map(person => (
@@ -23,7 +22,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList/>
+      {/* 1º Adicionado a propriedade props no component
+      ContactList para configurar cada elemento da lista
+      dinamicamente */}
+        <ContactList contacts={[
+          { name: 'Michael' },
+          { name: 'Ryan' },
+          { name: 'Tyler' }
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Maria' },
+          { name: 'Larissa' }
+        ]}/>
       </div>
     );
   }
