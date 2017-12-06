dfb5bf47c2dcaa587bbc3e2bfc41ca9a5b9ec1a4
Esteves <esteves@edusites.com.br>
2017-12-06 19:47:42 -0200

    Adicionado o gerenciamento de estado movendo a variavel contacts para dentro da propriedade state do component APP, como está dentro de um componente especifico agora ele será capaz de gerenciar o estado a cada atualização na view do component note também que o acesso ao objeto contacts mudou pois agora ele foi adicionado ao gerenciador de estado 'state' e seu acesso se dá this.state.contacts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
-
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
+ 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
   render() {
     return (
       <div>
         {/* Adicionando um atributo ao component ListContacts
-        e setando seu valor com um array de objetos contacts  */}
-        <ListContacts contacts={contacts}/>
+        e setando seu valor com um array de objetos contacts
+        OBS: Podemos pensar em passar os props para componentes,
+        assim como passamos argumentos para funções. Assim como
+        podemos acessar os argumentos passados ​​para uma função
+        em JavaScript, podemos também acessar os props de um
+        componente com this.props (ou props em componentes
+        funcionais sem estado).  */}
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     )
   }
