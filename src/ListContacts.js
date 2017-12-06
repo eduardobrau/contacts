cc193e42291a385add9b6e3ab33855f592cf20ed
Esteves <esteves@edusites.com.br>
2017-12-06 18:51:08 -0200

    Adicionado imagem do avatar e detalhes dentro da lista, obs:Foi preciso atribuir uma unique key prop e isso foi adicionado a <li key={contact.id}

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,9 +5,34 @@ class ListContacts extends Component{
   // a propriedade render() é a única obrigatoria
   // em um Component React, se não dá erro
   render(){
-    console.log('Props', this.props);
+    //console.log('Props', this.props);
     return(
       <ol className='contact-list'>
+      {/* 
+       Adicionando colchetes na expressao. 
+       Caso não declaramos uma unique 'key'
+       para cada filho do array veremos esse erro
+       Warning: Each child in an array or iterator should
+       have a unique "key" prop.
+       OBS: precisamos declarar a key porque estes itens
+       pode mudar, e caso mude o React saberá qual item
+       mudou sem precisar recriar todos os itens novamente.
+      */}
+      {this.props.contacts.map((contact) =>(
+        <li key={contact.id} className='contact-list-item'> 
+          <div className='contact-avatar' style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }}/>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button className='contact-remove'>
+            Remove
+          </button> 
+        </li>
+        
+      ))}
       </ol>
     )
   }
