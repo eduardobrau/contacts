3b4b6b12818491abe7960ab8e20e58c04d114e35
Esteves <esteves@edusites.com.br>
2017-12-06 19:03:42 -0200

    Transformado a classe de componente em um component funcional sem estado para isso tranformei a classe em uma função em que requer o argumento props

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react';
 
-class ListContacts extends Component{
-  // Definindo a IU para este component
-  // a propriedade render() é a única obrigatoria
-  // em um Component React, se não dá erro
-  render(){
-    //console.log('Props', this.props);
-    return(
-      <ol className='contact-list'>
-      {/* 
-       Adicionando colchetes na expressao. 
-       Caso não declaramos uma unique 'key'
-       para cada filho do array veremos esse erro
-       Warning: Each child in an array or iterator should
-       have a unique "key" prop.
-       OBS: precisamos declarar a key porque estes itens
-       pode mudar, e caso mude o React saberá qual item
-       mudou sem precisar recriar todos os itens novamente.
-      */}
-      {this.props.contacts.map((contact) =>(
-        <li key={contact.id} className='contact-list-item'> 
-          <div className='contact-avatar' style={{
-            backgroundImage: `url(${contact.avatarURL})`
-          }}/>
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
-          </div>
-          <button className='contact-remove'>
-            Remove
-          </button> 
-        </li>
-        
-      ))}
-      </ol>
-    )
-  }
-
+function ListContacts(props){
+  // Se um componente estiver usando apenas o método render para
+  // exibir conteúdo, então ele pode ser convertido em um 
+  // componente funcional sem estado. 
+  //console.log('Props', this.props);
+  return(
+    <ol className='contact-list'>
+    {/* 
+      Adicionando colchetes na expressao. 
+      Caso não declaramos uma unique 'key'
+      para cada filho do array veremos esse erro
+      Warning: Each child in an array or iterator should
+      have a unique "key" prop.
+      OBS: precisamos declarar a key porque estes itens
+      pode mudar, e caso mude o React saberá qual item
+      mudou sem precisar recriar todos os itens novamente.
+    */}
+    {props.contacts.map((contact) =>(
+      <li key={contact.id} className='contact-list-item'> 
+        <div className='contact-avatar' style={{
+          backgroundImage: `url(${contact.avatarURL})`
+        }}/>
+        <div className='contact-details'>
+          <p>{contact.name}</p>
+          <p>{contact.email}</p>
+        </div>
+        <button className='contact-remove'>
+          Remove
+        </button> 
+      </li>
+      
+    ))}
+    </ol>
+  )
 
 }
 
