9d99b76cd6c0934b7ef006ed04a339e56455532f
Esteves <esteves@edusites.com.br>
2017-12-07 11:35:44 -0200

    Criado função removeContact para atualizar o estado do component APP para isso foi passado como argumento o estado atual do component para fazer um marge com o retorno da nova lista de contatos já filtrada, ou seja removendo da nova lista o contact passado a função

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts';
  
 class App extends Component {
+  /* Definido o estado inicial do component APP com
+  o array de objetos contacts */
   state = {
     contacts: [
       {
@@ -24,6 +26,22 @@ class App extends Component {
       }
     ]
   }
+  // função para remover um contato especifico
+  removeContact = (contact) => {
+    /* setState atualizara o estado atual do Component
+    removendo um contact especifico ao clickar no botão 
+    do component ListContacts.
+    */
+    this.setState((state) => ({
+      /* Usamos setState com uma arow function como argumento
+      porque queremos atualizar o estado com base no anterior,
+      ou seja, vamos remover um contato e retornar uma nova
+      lista de contato com base nos contatos já existente
+      assim atualizando o estado do Component sempre que o
+      botão remover for clicado.*/
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
   render() {
     return (
       <div>
@@ -35,7 +53,7 @@ class App extends Component {
         em JavaScript, podemos também acessar os props de um
         componente com this.props (ou props em componentes
         funcionais sem estado).  */}
-        <ListContacts contacts={this.state.contacts}/>
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
       </div>
     )
   }
