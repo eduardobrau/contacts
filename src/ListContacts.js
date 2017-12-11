fb3927c21d1ab3bf1bde71500723aef74df8eb3d
Esteves <esteves@edusites.com.br>
2017-12-11 18:25:34 -0200

    ListContacts convertido em um compoente de classe com inicialização de estado na propriedade query onde toda vez que for digitado algo no campo de busca será encadeado um evento updateQuery que atualizara o novo estado do componente com o valor digitado

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,46 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-function ListContacts(props){
+class ListContacts extends Component{
   // Se um componente estiver usando apenas o método render para
   // exibir conteúdo, então ele pode ser convertido em um 
   // componente funcional sem estado. 
   //console.log('Props', this.props);
-  return(
-    <ol className='contact-list'>
-    {/* 
-      Adicionando colchetes na expressao. 
-      Caso não declaramos uma unique 'key'
-      para cada filho do array veremos esse erro
-      Warning: Each child in an array or iterator should
-      have a unique "key" prop.
-      OBS: precisamos declarar a key porque estes itens
-      pode mudar, e caso mude o React saberá qual item
-      mudou sem precisar recriar todos os itens novamente.
-    */}
-    {props.contacts.map((contact) =>(
-      <li key={contact.id} className='contact-list-item'> 
-        <div className='contact-avatar' style={{
-          backgroundImage: `url(${contact.avatarURL})`
-        }}/>
-        <div className='contact-details'>
-          <p>{contact.name}</p>
-          <p>{contact.email}</p>
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+  state={
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+  render(){
+    return(
+      <div className='list-contacts'>
+        {JSON.stringify(this.state)}
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
         </div>
-        <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-          Remove
-        </button> 
-      </li>
-      
-    ))}
-    </ol>
-  )
-
-}
-
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+        <ol className='contact-list'>
+        {/* 
+          Adicionando colchetes na expressao. 
+          Caso não declaramos uma unique 'key'
+          para cada filho do array veremos esse erro
+          Warning: Each child in an array or iterator should
+          have a unique "key" prop.
+          OBS: precisamos declarar a key porque estes itens
+          pode mudar, e caso mude o React saberá qual item
+          mudou sem precisar recriar todos os itens novamente.
+        */}
+        {this.props.contacts.map((contact) =>(
+          <li key={contact.id} className='contact-list-item'> 
+            <div className='contact-avatar' style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              Remove
+            </button> 
+          </li>
+          
+        ))}
+        </ol>
+      </div>
+    )
+  };
 }
 
 export default ListContacts;
