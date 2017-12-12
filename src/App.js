cb113b1f96a1fd2c1da114c3910af29d65f7ca02
Esteves <esteves@edusites.com.br>
2017-12-12 00:10:40 -0200

    Excluido dados do contacts e inicialado como um array vazio, fazendo uma busca remota aos dados com o componentDidMount() que logo que a página é renderizada e forma sua arvore DOM, uma requisição é feita e setState atualiza o estado com os dados retornado

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
  
 class App extends Component {
   /* Definido o estado inicial do component APP com
   o array de objetos contacts */
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+  // Evento do ciclo de vida ou gancho que é disparado
+  //sempre que os nós DOM estão prontos em uma página
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
   }
   // função para remover um contato especifico
   removeContact = (contact) => {
