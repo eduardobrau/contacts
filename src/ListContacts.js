a1357ba1b1b38e8a2a603c5ddd04ce8593e8be89
Esteves <esteves@edusites.com.br>
2017-12-12 11:40:38 -0200

    Adicionado botão com link para add user, para quando clicar atualizar o estado screen para create através da expressão de função onNavigate que precisou estar em App.js já que o estado screen do aplicativo está lá e o que queremos é que ao clickar no botão a função atualize o estado screen, assim atualizando o estado do aplicativo executa novamente seu método render() e executar novamente a condição this.state.screeen === 'create'

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -53,6 +53,11 @@ class ListContacts extends Component{
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <a
+            href='#create'
+            onClick={this.props.onNavigate}
+            className='add-contact'
+          >Add Contact</a>
         </div>
         {/* 
         Caso o usuário digitar algo então será renderizado 
