351e85aa084dfb4c2c68a313b13d868a05f94333
Esteves <esteves@edusites.com.br>
2017-12-13 09:54:36 -0200

    Importado component serializeForm e adicionado um evento onSubmit no formulario CreateContact mudando o comportamento padrão de submeter o formulário e serializando os dados ao enviar os dados e caso foi definido uma Props onCreactContact(contact) no component pai então executar this.props.onCreatContact(values) que encapsula uma função creactContact(contact) definida no component pai

--- src/App.js
+++ src/App.js
@@ -35,6 +35,13 @@ class App extends Component {
     // Agora além de apagar os dados local estamos apagando do DB
     ContactsAPI.remove(contact)
   }
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
   render() {
     return (
       <div>
@@ -53,7 +60,14 @@ class App extends Component {
           />
         )}/>
         
-        <Route path='/create' component={CreateContact}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
          
       </div>
     )
